482e89216e7c919bb517cd1405bba4346d66c8d1
Anuu_17 <pawar@teradoga.com>
2025-02-18 16:28:36 +0900

    計算ボタン削除

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -245,7 +245,7 @@
                                                         <span class="badge badge-pill bg-green-lt ms-3"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-star"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>顧客満足度：4.9/5</span>
 
                                                         <div class="ms-auto">
-                                                            
+
                                                         </div>
 
                                                 </div>
@@ -350,7 +350,7 @@
                         <div class="chat-footer">
                             <div class="input-group">
                              <textarea id="new-instruction" class="form-control" rows="1" placeholder="メッセージ入力しましょう。"></textarea>
-                             <button id="calculateToken" class="btn btn-green">計算</button><button id="send-button" class="btn btn-primary">送信</button>
+                             <button id="send-button" class="btn btn-primary">送信</button>
                             </div>
                         </div>
                     `);
@@ -525,7 +525,7 @@
                     <div class="chat-footer">
                         <div class="input-group">
                             <textarea id="new-instruction" class="form-control" rows="1" placeholder="メッセージ入力しましょう。"></textarea>
-                            <button id="calculateToken" class="btn btn-green">計算</button><button id="send-button" class="btn btn-primary">送信</button>
+                            <button id="send-button" class="btn btn-primary">送信</button>
                         </div>
                     </div>
                 `);
